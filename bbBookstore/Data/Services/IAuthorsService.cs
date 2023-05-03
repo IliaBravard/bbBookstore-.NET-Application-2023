24c24a921aad7707348bbf8338d173c95ad818ff
AngelBerber <115816285+AngelBerber@users.noreply.github.com>
2023-05-03 17:32:19 -0500

    Add files via upload

---
+++ bbBookstore/Data/Services/IAuthorsService.cs
@@ -0,0 +1,9 @@
+﻿using bbBookstore.Data.Base;
+using bbBookstore.Models;
+
+namespace bbBookstore.Data.Services
+{
+    public interface IAuthorsService : IEntityBaseRepository<Author>
+    {
+    }
+}
